4f8c3d9a7fd2ebaa26a93123bf3af19c3350d2aa
NatanVachon <47736713+NatanVachon@users.noreply.github.com>
2019-03-19 15:58:15 +0100

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 10:18:17 2019
+
+This file contains everything about predict labels on raw data in order to
+train NNs.
+
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Detection parameters
+"""
+MOVING_AVERAGE_NB = 19
+
+#SHOCK_TOTELS_THRESHOLD = 5e4 # nb of shocks
+SHOCK_TOTELS_THRESHOLD_RATIO = 0.2
+SOLAR_WIND_DELTA_THRESHOLD = 50 # km/s
+
+"""
+Semi-automatic prediction of labels for every timestamp in the dataframe.
+To determine the label we assume that in an hour we can detect one cross
+
+TODO: VERIFIER LES INDICES
+
+Arguments:
+    pandas.DataFrame() containing data for several timestamps
+
+Returns:
+    Input dataframe with an additional column containing labels if the direction
+    is detected, else return None
+"""
+def predict_file(data):
+    # Fill nan values with median values
+    data = fill_nan_values(data)
+    # Compute smoothed energy
+    smoothed_totels = moving_average(np.nan_to_num(np.array(data["totels_1"])), MOVING_AVERAGE_NB)
+    # Compute shock boundaries
+    begin_shock, end_shock = compute_boundary_indexes(smoothed_totels, SHOCK_TOTELS_THRESHOLD_RATIO)
+    # Split data in three parts
+    first_part = data.iloc[:begin_shock].copy()
+    last_part = data.iloc[end_shock:].copy()
+    # Compute direction
+    direction = compute_direction(first_part, last_part)
+    # Check for label detection
+    if direction == 1:
+        print("Direction not found")
+        return None
+    # Assign label
+    data['label'] = [0 for i in range(data.count(0)[0])]
+    data['label'].iloc[:begin_shock] = [direction for i in range(begin_shock)]
+    data['label'].iloc[begin_shock:end_shock] = [1 for i in range(end_shock - begin_shock)]
+    data['label'].iloc[end_shock:] = [(2 - direction) for i in range(len(data['label'].iloc[end_shock:]))]
+    return data
+
+"""
+Replaces nan values with median value
+Input:
+    pandas.DataFrame()
+
+Returns:
+    pandas.DataFrame() with no more nan values
+"""
+def fill_nan_values(data):
+    data_copy = data.copy()
+    for i in range(1, data_copy.count(1)[0]):
+        data_copy.iloc[:,i] = data_copy.iloc[:,i].fillna(data_copy.iloc[:,i].median())
+    return data_copy
+
+"""
+Determines if the satellite is ascending or descending
+
+Arguments:
+    pandas.DataFrame(), First period data
+    pandas.DataFrame(), Last period data
+
+Returns:
+    0 if ascending
+    1 if not sure
+    2 if descending
+"""
+def compute_direction(first_part, last_part):
+#    mean_deriv_r_1 = np.mean(first_part["deriv_r"])
+##    print('der_r_1 = ' + str(mean_deriv_r_1))
+#    mean_deriv_r_2 = np.mean(last_part["deriv_r"])
+##    print('der_r_2 = ' + str(mean_deriv_r_2))
+    mean_sw_vel_1 = np.mean(first_part["SWIA_vel_x"])
+    print('sw_vel_1 = ' + str(mean_sw_vel_1))
+    mean_sw_vel_2 = np.mean(last_part["SWIA_vel_x"])
+    print('sw_vel_2 = ' + str(mean_sw_vel_2))
+
+#    if mean_deriv_r_1 > 0 and mean_deriv_r_2 > 0 and mean_sw_vel_1 > SOLAR_WIND_THRESHOLD and mean_sw_vel_2 <= SOLAR_WIND_THRESHOLD:
+#        return 0
+#    elif mean_deriv_r_1 < 0 and mean_deriv_r_2 < 0 and mean_sw_vel_1 <= SOLAR_WIND_THRESHOLD and mean_sw_vel_2 > SOLAR_WIND_THRESHOLD:
+#        return 2
+#    else:
+#        return 1
+    if mean_sw_vel_1 - mean_sw_vel_2 > SOLAR_WIND_DELTA_THRESHOLD:
+        return 0
+    elif mean_sw_vel_1 - mean_sw_vel_2 < SOLAR_WIND_DELTA_THRESHOLD:
+        return 2
+    else:
+        return 1
+
+"""
+Function used to plot several variables in order to choose biases to estimate
+label
+
+Argument:
+    pandas.DataFrame() containing data for several timestamps
+"""
+def plot_variables(data):
+    time = pd.to_datetime(data["epoch"])
+    smoothed_totels = moving_average(np.nan_to_num(np.array(data["totels_1"])), MOVING_AVERAGE_NB)
+    begin_shock, end_shock = compute_boundary_indexes(smoothed_totels, SHOCK_TOTELS_THRESHOLD_RATIO)
+
+    # raw data
+    plt.subplot(2, 1, 1)
+    plt.plot(time, data["totels_1"], 'b-')
+    plt.plot(time, data["totels_8"], 'r-')
+
+    # ma data
+    plt.subplot(2, 1, 2)
+    plt.plot(time, smoothed_totels, 'b-')
+    plt.plot([time[begin_shock]] * 2, [0, max(smoothed_totels)], 'g--')
+    plt.plot([time[end_shock]] * 2, [0, max(smoothed_totels)], 'k--')
+
+    plt.show()
+
+"""
+Math utilitiy functions
+"""
+"""
+Function used to detect a band of activity in which a certain data is higher
+than a certain threshold with some additional feature to make detection more
+stable
+
+Input:
+    List of values for the analyzed data
+    Ratio threshold (detection based on the maximum value of the list)
+
+Returns:
+    Boundaries of the activity band
+"""
+def compute_boundary_indexes(data, threshold_ratio):
+    data_copy = np.array(data.copy())
+    data_copy -= min(data_copy)
+    maxi = max(data_copy)
+    first_occurence = next(i for i, x in enumerate(data_copy) if x >= (maxi * threshold_ratio))
+    data_copy = np.flip(data_copy)
+    last_occurence = len(data_copy) - 1 - next(i for i, x in enumerate(data_copy) if x >= (maxi * threshold_ratio))
+    return first_occurence, last_occurence
+
+def moving_average(data, n):
+    raw_data = data.copy();
+    N = len(raw_data)
+    ma_data = []
+    for i in range(N):
+        sum = 0.0
+        for k in range(int(-n/2), int(n/2) + 1):
+            sum += raw_data[saturate(i + k, 0, N - 1)]
+        ma_data.append(float(sum) / n)
+    return ma_data
+
+def saturate(x, mini, maxi):
+    return mini if x < mini else maxi if x > maxi else x
---
+++ preprocessing.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 11:12:13 2019
+
+Preprocessing file. The following functions are used to prepare datasets to be
+used for training Neural Networks
+
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from keras.utils import np_utils
+
+"""
+Getting the train and test sets
+Arguments:
+    timed_data : a given pandas.DataFrame() to convert to test and train sets, containing at least a 'label' and an 'epoch' columns
+    ordered : defines if the data sets created are shuffled (default: ordered=False) or chronological (ordered=True).
+    test_size : proportion of samples included in the test set (default: 0.3)
+
+    nb_class : defines if the problem has to be downgraded to 3 classes instead of 4
+
+Return:
+    X_train_timed, y_train_timed, X_test_timed, y_test_timed
+    (These variables are called '_timed' because they still contain the 'epoch' information of the initial data)
+"""
+def get_timed_train_test(timed_data, test_size = 0.3, nb_class = 3):
+
+    ###########################
+    #this part is specific to the martian problem
+    ###########################
+    if nb_class == 3 :
+        labels = np.ravel(timed_data['label'])
+        timed_data['label'] = labels
+    elif nb_class != 4 :
+        print("WARNING: wrong number of classes")
+        return None
+    ############################
+    y = pd.DataFrame()
+    y['epoch'] = timed_data['epoch']
+    y['label'] = timed_data['label']
+
+    X = timed_data.copy()
+    del X['label']
+
+    X_train_timed, X_test_timed, y_train_timed, y_test_timed = train_test_split(X,y,test_size = test_size)
+
+    return X_train_timed, X_test_timed, y_train_timed, y_test_timed
+
+"""
+Getting the train and test sets without any time info and apply the scaling here
+"""
+def get_train_test_sets(X_train_timed, X_test_timed, y_train_timed, y_test_timed):
+    X_train = X_train_timed.copy()
+    X_test = X_test_timed.copy()
+
+    y_train = one_hot_encode(y_train_timed['label'].tolist())
+    y_test =  one_hot_encode(y_test_timed['label'].tolist())
+
+    del X_train['epoch']
+    del X_test['epoch']
+
+    scaler = StandardScaler().fit(X_train)
+    # Scale the train and test set
+    X_train = scaler.transform(X_train)
+    X_test  = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
+
+"""
+Useful functions to switch between labels representations
+    The 2 different representations are represented here for an example with 3 distinct classes
+
+    labels = [0, 0, 1, 2, 1, 0, 2, 2, 0]
+
+    y = [[1, 0, 0],
+         [1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1],
+         [0, 1, 0],
+         [1, 0, 0],
+         [0, 0, 1],
+         [0, 0, 1],
+         [1, 0, 0]]
+
+"""
+from sklearn.preprocessing import LabelEncoder
+
+def one_hot_encode(labels):
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+    encoded = encoder.transform(labels)
+    # convert integers to dummy variables (i.e. one hot encoded)
+    y = np_utils.to_categorical(encoded)
+    return y
